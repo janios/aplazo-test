6386137815484a62ec66a482dc236bbbdb682861
Cristian <escamilla.cristian@gmail.com>
2022-05-03 16:56:45 -0500

    Initial commit

---
+++ src/main/java/com/cristian/aplazotest/AplazoTestApplication.java
@@ -0,0 +1,13 @@
+package com.cristian.aplazotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplazoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplazoTestApplication.class, args);
+	}
+
+}
